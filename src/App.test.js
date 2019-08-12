2fc1d2dbdbeca300fc930bae5e7201605e5e3408
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2019-08-12 21:14:33 +0100

    Created initial React app framework

---
+++ src/App.test.js
@@ -0,0 +1,3 @@
+/* global it */
+
+it("is a placeholder", () => undefined);
---
+++ src/main.js
@@ -0,0 +1,46 @@
+/* global require process path __dirname */
+import { app, BrowserWindow } from "electron";
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
+    app.quit();
+}
+
+// Keep a global reference of the window object
+let window;
+
+const createWindow = () => {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    // Run the live-reload enabled version for dev environment
+    if (process.env.ENVIRONMENT === "development") {
+        window.loadURL("http://localhost:3000");
+    } else {
+        window.loadFile(path.join(__dirname, "/../build/index.html"));
+    }
+
+    window.on("closed", () => {
+        // Dereference the window object
+        window = null;
+    });
+};
+
+// Create the app window once Electron is ready
+app.on("ready", createWindow);
+
+// Quit when all windows are closed. On OS X it is common for applications and
+// their menu bar to stay active until the user quits explicitly with Cmd + Q
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (window === null) {
+        createWindow();
+    }
+});
